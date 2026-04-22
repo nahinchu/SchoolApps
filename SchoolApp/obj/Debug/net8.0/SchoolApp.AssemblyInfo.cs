4def0caddf73cf13fbb64e4bf3362adf84a9eff1
nahinchu <hoangnahinchu5504@gmail.com>
2026-04-22 17:23:34 +0700

    css homePage

--- SchoolApp/obj/Debug/net8.0/SchoolApp.AssemblyInfo.cs
+++ SchoolApp/obj/Debug/net8.0/SchoolApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e552106a4584d3f22311b1cc234e8871abb77d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
