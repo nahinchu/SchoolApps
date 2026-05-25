a50727e6fda3268553af5bde1577265c1ecdbc6e
nahinchu <hoangnahinchu5504@gmail.com>
2026-05-25 08:50:51 +0700

    excel

---
+++ SchoolApp/Services/ExcelExportService/IExcelExportService.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolApp.Services.ExcelExportService
+{
+    public interface IExcelExportService
+    {
+        byte[] Export<T>(
+            IEnumerable<T> data,
+            IEnumerable<ExcelColumnDef<T>> columns,
+            string sheetName = "Sheet1",
+            string? title = null);
+    }
+}
